fbafb32d386175f5bb946065a4587300ed7bc9e7
Emer Arteaga <arteagaemer@gmail.com>
2022-11-11 16:37:05 -0500

    Inicio del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import MyNavBar from './components/navbar/NavBarPersonalized.js';
+import logo from  './particl.svg';
+import { Route, Routes } from 'react-router-dom';
+import { ProductoListComponent } from './components/products/ProductoList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNavBar src={logo}></MyNavBar>
+      <Routes>
+        <Route path='/' element={<ProductoListComponent></ProductoListComponent>}>
+        </Route>
+        <Route path='/products' element={
+          <h1>Hola desde products</h1>
+        }>
+        </Route>
+      </Routes>
     </div>
   );
 }
