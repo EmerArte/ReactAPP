5c6715a08bac5a182102e6706d46be542299c80a
Carlos Galvis Padilla <carlos.galvis@sofka.com.co>
2022-11-13 16:53:50 -0500

    intento de sprint 3 xd

---
+++ src/services/ClientService.js
@@ -0,0 +1,30 @@
+import json from "./products.json";
+
+
+let arrayProductSelect;
+
+export function listProductAvailable(products) {
+    products.forEach((value, index,) => {
+            if (value.stock > 0) {
+                console.log("producto " + index + " " + value)
+            }
+        }
+    )
+}
+
+export function selectProducts(product) {
+    arrayProductSelect.push(product);
+}
+
+export function checkout() {
+    let totalValue;
+    arrayProductSelect.forEach(
+        (object) => {
+            totalValue += object.price
+            json.products.map(product =>
+                object.id === product.id ? {...product, stock: product.stock - object.amount} : product)
+        }
+    )
+    console.log(totalValue)
+}
+
