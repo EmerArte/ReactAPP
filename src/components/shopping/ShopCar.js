e5539801c965f5556f40e7f41b1e332517652f1c
Emer Arteaga <arteagaemer@gmail.com>
2022-11-15 22:18:02 -0500

    CheckOut

--- src/components/shopping/ShopCar.js
+++ src/components/shopping/ShopCar.js
@@ -1,8 +1,10 @@
-import { Container, Table } from "react-bootstrap";
-import { ShopContext } from "../../App";
+import { Button, Container, Table } from "react-bootstrap";
+import { ShopContext, Context } from "../../App";
 import React, { useContext } from "react";
-export function ShopCar(props) {
+export function ShopCar() {
   const carritoCompra = useContext(ShopContext);
+  const productos = useContext(Context);
+  let total = 0;
   return (
     <Container className="my-3">
       <Table striped bordered hover>
@@ -18,6 +20,7 @@ export function ShopCar(props) {
         <tbody>
           {
             carritoCompra.map((producto)=>{
+              total += producto.price * producto.cantidad; 
               return (
               <tr key={producto.id}>
                 <td><img src={producto.images[0]}
@@ -33,6 +36,23 @@ export function ShopCar(props) {
           }
         </tbody>
       </Table>
+      <div className="d-flex flex-row justify-content-between">
+      <h3 className="fs-6 w-75 text-end">Total a pagar: </h3>
+      <h3 className="fs-6 fst-italic w-25 text-end">${total}</h3>
+      </div>
+      <div className="d-flex flex-row-reverse my-3">
+        <Button onClick={()=>{Checkout(carritoCompra,productos)}} disabled={carritoCompra.length > 0 ? false : true} >CheckOut</Button>
+      </div>
     </Container>
   );
+}
+
+function Checkout(carritoCompra, productos){
+  carritoCompra.forEach(element => {
+    const productoIndex = productos.findIndex((value)=>value.id === element.id);
+    if(productoIndex !== -1){
+      productos[productoIndex].stock = productos[productoIndex].stock - element.cantidad;
+      console.log("Producto: " + element.title + " quedan : " + productos[productoIndex].stock + " en Stock")
+    }
+  }); 
 }
